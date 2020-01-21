61a7e7ddfd06d2659208f24140f83e1d9bccd87e
Vladimir Arias-Antonov <ariasantonov93@gmail.com>
2020-01-21 15:21:53 -0800

    css added

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,10 +1,52 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>My-PWP-Portfolio</title>
+	<link rel="stylesheet" type="text/css" href="./style.css">
 </head>
 <body>
 
+	<h2>PWP Portfolio Mile Stone 1</h2>
+	<p><strong>Purpose:</strong> Create a professional portfolio to showcase my skills and projects to employers.
+	</p>
+
+	<p><strong>Audience:</strong>Freelance Clients and Software Dev Companies Looking for Software Developers.
+	</p>
+
+	<p><strong>Goal:</strong></p>
+	<ul>
+		<li>Build an attractive portfolio to showcase my work.</li>
+		<li>To acquire full or part-time employment or an internship..</li>
+		<li>To attract freelance clients.</li>
+	</ul>
+
+
+<h2><strong>Persona:</strong></h2>
+<img class="coach" src="https://images.unsplash.com/photo-1529165980561-f19d4acc4f3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"></img>
+	<ul>
+		<li><strong>Name:</strong> Max Payne</li>
+		<li><strong>Age:</strong> 54</li>
+		<li><strong>Gender:</strong> Male</li>
+		<li><strong>Ethnicity:</strong> White</li>
+		<li><strong>Occupation:</strong> Boxing Gym Coach/Owner</li>
+		<li><strong>Technology:</strong> <em>Phone</em>; Iphone 7 / Gym Land Line. <em>Desktop PC</em> <em>OS</em>: Windows 8.1</li>
+		<li><strong>Attitude:</strong> Wants to promote classes and schedule, likes upload pictures, and have an easy access to social media.</li>
+		<li><strong>Needs/Goals/Features:</strong></li>
+		<ul>
+			<li>Needs to promote the gym and attract new Members.</li>
+			<li>Needs an easy way to sign users up with user contact info.</li>
+			<li>Needs an easy/intuitive way to navigate site and access info and resources.</li>
+			<li>Wants a visually attractive site with plenty of pictures and social media integration. </li>
+			<li>Needs Mobile support. Not just PC. </li>
+		</ul>
+		<li><strong>Frustrations:</strong></li>
+		<ul>
+			<li>Image gallery that doesnt work.</li>
+			<li>Hard to navigate website.</li>
+			<li>No Mobile support.</li>
+			<li>Unable to sign users up and easily send out promotions and announcements.</li>
+		</ul>
+	</ul>
 </body>
 </html>
