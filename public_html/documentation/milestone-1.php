064a4e9655a14b28e191f2296e17707a4c91492b
Vladimir Arias-Antonov <ariasantonov93@gmail.com>
2020-01-22 16:30:58 -0800

    fixed strat img tag

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -23,16 +23,18 @@
 
 
 <h2><strong>Persona:</strong></h2>
-<img class="coach" src="https://images.unsplash.com/photo-1529165980561-f19d4acc4f3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"></img>
-	<ul>
-		<li><strong>Name:</strong> Max Payne</li>
-		<li><strong>Age:</strong> 54</li>
-		<li><strong>Gender:</strong> Male</li>
-		<li><strong>Ethnicity:</strong> White</li>
-		<li><strong>Occupation:</strong> Boxing Gym Coach/Owner</li>
-		<li><strong>Technology:</strong> <em>Phone</em>; Iphone 7 / Gym Land Line. <em>Desktop PC</em> <em>OS</em>: Windows 8.1</li>
-		<li><strong>Attitude:</strong> Wants to promote classes and schedule, likes to upload pictures, and have an easy access to social media.</li>
-		<li><strong>Needs/Goals/Features:</strong></li>
+<img class="coach" src="https://images.unsplash.com/photo-1529165980561-f19d4acc4f3f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" alt="boxer">
+
+		<ul>
+			<li><strong>Name:</strong> Max Payne</li>
+			<li><strong>Age:</strong> 54</li>
+			<li><strong>Gender:</strong> Male</li>
+			<li><strong>Ethnicity:</strong> White</li>
+			<li><strong>Occupation:</strong> Boxing Gym Coach/Owner</li>
+			<li><strong>Technology:</strong> <em>Phone</em>; Iphone 7 / Gym Land Line. <em>Desktop PC</em> <em>OS</em>: Windows 8.1</li>
+			<li><strong>Attitude:</strong> Wants to promote classes and schedule, likes to upload pictures, and have an easy access to social media.</li>
+			<li><strong>Needs/Goals/Features:</strong></li>
+
 		<ul>
 			<li>Needs to promote the gym and attract new Members.</li>
 			<li>Needs an easy way to sign users up with user contact info.</li>
@@ -40,17 +42,19 @@
 			<li>Wants a visually attractive site with plenty of pictures and social media integration. </li>
 			<li>Needs Mobile support. Not just PC. </li>
 		</ul>
+
 		<li><strong>Frustrations:</strong></li>
 		<ul>
 			<li>Image gallery that doesnt work.</li>
 			<li>Hard to navigate website.</li>
 			<li>No Mobile support.</li>
 			<li>Unable to sign users up and easily send out promotions and announcements.</li>
-		</ul>
-		<li><strong>User Story:</strong></li>
-		<ul>
+			</ul>
+			<li><strong>User Story:</strong></li>
+			<ul>
 			<li>As an inexperienced/standard user, Max Wants an aesthetically pleasing website that is easy to navigate and link to social media.</li>
+			</ul>
 		</ul>
-	</ul>
+
 </body>
 </html>
