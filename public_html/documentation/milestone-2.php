570ea8e99f05248eb4a60904b69b1a6b23d02713
Vladimir Arias-Antonov <ariasantonov93@gmail.com>
2020-02-04 13:54:41 -0800

    added Mile Stone 2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Content Strategy</title>
+</head>
+<body>
+<h1><strong>Content Strategy</strong></h1>
+<div>
+	<ul>
+		<strong>My site will be one page, and will consist of the following sections:</strong>
+		<li><strong>Welcome:</strong> A brief one-line introduction that clearly states my name and role as a Front End Developer. Background image will be a stock photo of a laptop and coffee. Dark colors / theme.
+
+		<li><strong>About Me:</strong> This section will detail my professional history, core skill set, and professional highlights. This section will showcase my strengths and personality. Will stress my desire to attract employers as well as individual contract work.
+
+		<li><strong>Portfolio:</strong> I want this to be an animated, responsive image gallery containing 4-6 examples of my best work. Will have interactive and responsive pictures/animations reflecting my use of css/java script.
+
+		<li><strong>Contact:</strong> This section will contain a simple contact form, my professional email address. Social Media Links: LinkedIn, Twitter, GitHub. Bootstrap links/icons at footer.
+	</ul>
+</div>
+<div>
+	<img src="pwpWireframe.png" alt="wireframe">
+</div>
+</body>
+</html>
